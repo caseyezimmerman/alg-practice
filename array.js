dd114449ec03bfe716b5b7e9c2867fd984c18361
Casey Zimmerman <caseyezimmerman@gmail.com>
2018-01-23 11:38:45 -0500

    more practice

---
+++ array.js
@@ -0,0 +1,8 @@
+/////adding numbers in an array
+
+var number = [1,2,3,4,5,6,7,8]
+total = 0
+	for (let i = 0; i < number.length; i ++){
+		total += number[i];
+	}
+console.log(total)
---
+++ return.js
@@ -0,0 +1,11 @@
+//////when to use return
+
+function test(){
+	var number = 1 + 1;
+	return number
+}
+
+var variable = test()
+console.log(variable)
+
+
